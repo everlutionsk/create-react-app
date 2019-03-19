be7fc83b68cc01b98dbce0ad38b188c6516969de
Tomáš Szabo <tomas.szabo@deftomat.com>
2019-03-19 16:09:01 +0100

    disable version verification for babel-jest, eslint

--- packages/react-scripts/scripts/utils/verifyPackageTree.js
+++ packages/react-scripts/scripts/utils/verifyPackageTree.js
@@ -21,9 +21,9 @@ function verifyPackageTree() {
     // See https://github.com/facebook/create-react-app/issues/1795 for reasons why.
     // I have not included Babel here because plugins typically don't import Babel (so it's not affected).
     'babel-eslint',
-    'babel-jest',
+    // 'babel-jest',
     'babel-loader',
-    'eslint',
+    // 'eslint',
     // 'jest',
     // 'webpack',
     'webpack-dev-server',
