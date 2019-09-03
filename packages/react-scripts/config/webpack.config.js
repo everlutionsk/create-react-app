5370d2bd0e0d912d5d3c9e2d4c4e8304401900cc
Tomáš Szabo <tomas.szabo@deftomat.com>
2019-09-03 09:39:05 +0200

    feat: skip `waiting` state in service-workers
    Updated service worker will be activated immediatelly instead of waiting until all tabs are closed.

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -696,6 +696,7 @@ module.exports = function(webpackEnv) {
       isEnvProduction &&
         new WorkboxWebpackPlugin.GenerateSW({
           clientsClaim: true,
+          skipWaiting: true,
           exclude: [/\.map$/, /asset-manifest\.json$/],
           importWorkboxFrom: 'cdn',
           navigateFallback: publicUrl + '/index.html',
